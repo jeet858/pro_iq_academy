721f2b9123ade5832c82c2f975a2ee25ba08548a
Achinta27 <achinta27sep1999@gmail.com>
2023-09-21 21:19:32 +0530

    update

--- src/components/elements/FranchisingSection.tsx
+++ src/components/elements/FranchisingSection.tsx
@@ -1,21 +1,93 @@
 import React from "react";
 import Image from "next/image";
-
+import img from "../../../public/images/banner 25.jpg";
+import img1 from "../../../public/images/busy-female-teenager-reads-book-uses-table-lamp.jpg";
 const FranchisingSection = () => {
   return (
     <div className="h-fit">
       <div className="relative h-80 bg-cover bg-center bg-no-repeat">
         <div>
           <Image
-            src="/images/bi.jpg"
+            src="/images/22.jpg"
             alt="About Us"
             layout="fill"
             objectFit="cover"
           />
         </div>
-        <p className="relative ml-32 pt-32 text-2xl font-bold text-white">
+        <p className="relative ml-32 pt-32 font-[Prata] text-6xl text-white">
           Franchising
         </p>
+      </div>
+      <div className="sm:p-10 lg:p-20 ">
+        <p className="font-[Prata] sm:text-lg lg:text-2xl">
+          Franchising Opportunities with ProIQ Academy: At ProIQ Academy, we are
+          excited to offer franchising opportunities forindividuals who are
+          passionate about education andwant to make a positive impact in their
+          community. Joining our franchise network allows you to become a part
+          of our mission to provide high-quality education and shape the future
+          of learning. Here's why franchising with ProIQ Academy is a rewarding
+          venture:
+        </p>
+      </div>
+      <div className="h-fit w-full">
+        <Image src={img} alt="" />
+      </div>
+      <div className="flex flex-col sm:p-10 md:p-10 lg:p-20">
+        <div className="flex w-full items-center justify-between sm:flex-col md:flex-row">
+          <div className="h-fit w-fit">
+            <Image
+              alt=""
+              src={img1}
+              className="md:h-[266px] md:w-[380px] lg:h-[466px] lg:w-[580px]"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center sm:w-full lg:w-1/2">
+            <h1 className="font-[Prata] font-semibold text-[#233D63] md:text-xl lg:text-3xl">
+              Established Brand and Reputation:
+            </h1>
+            <p className="font-[Gilroy-Regular] text-[#233D63] md:text-base lg:text-lg">
+              ProIQ Academy has built a strong brand presence in the education
+              industry. By becoming a franchisee, you benefit from our
+              established reputation, credibility, and a proven track record of
+              success.
+            </p>
+          </div>
+        </div>
+        <div className="flex w-full flex-row items-center justify-between sm:flex-col-reverse md:flex-row">
+          <div className="flex flex-col gap-8 sm:w-full lg:w-1/2">
+            <div className="flex  flex-col items-center justify-center gap-4 ">
+              <h1 className="font-[Prata] font-semibold text-[#233D63] md:text-xl lg:text-3xl">
+                Comprehensive Support and Training:
+              </h1>
+              <p className="font-[Gilroy-Regular] text-[#233D63] md:text-base lg:text-lg">
+                With a vision to become a leading institute, we aim to inspire
+                admiration by spreading the values of education, integrity, and
+                fun through our young students. Our ultimate goal is to promote
+                skill-based education and ensure that every student receives the
+                necessary tools for success..
+              </p>
+            </div>
+            <div className="flex  flex-col items-center justify-center gap-4 ">
+              <h1 className="font-[Prata] font-semibold text-[#233D63] md:text-xl lg:text-3xl">
+                Comprehensive Support and Training:
+              </h1>
+              <p className="font-[Gilroy-Regular] text-[#233D63] md:text-base lg:text-lg">
+                With a vision to become a leading institute, we aim to inspire
+                admiration by spreading the values of education, integrity, and
+                fun through our young students. Our ultimate goal is to promote
+                skill-based education and ensure that every student receives the
+                necessary tools for success..
+              </p>
+            </div>
+          </div>
+          <div className="h-fit w-fit">
+            <Image
+              alt=""
+              src={img1}
+              className="md:h-[266px] md:w-[380px] lg:h-[466px] lg:w-[580px]"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/elements/OurCoursesSection.tsx
+++ src/components/elements/OurCoursesSection.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import img1 from "../../../public/images/oc2.jpg";
-import img2 from "../../../public/images/oc1.jpg";
-import img3 from "../../../public/images/oc3.jpg";
-import icon1 from "../../../public/images/knowledge.png";
-import icon2 from "../../../public/images/ai.png";
-import icon3 from "../../../public/images/python.png";
-import icon4 from "../../../public/images/analyst.png";
-import icon5 from "../../../public/images/deep-learning.png";
-import icon6 from "../../../public/images/data-science.png";
+import img1 from "../../../public/images/virtual-classroom-study-space.jpg";
+import img2 from "../../../public/images/medium-shot-graduate-student.jpg";
 
-import { BsArrowRight } from "react-icons/bs";
+import OurCoursesSkill from "./OurCoursesSkill";
+import { BiSolidCircle } from "react-icons/bi";
+
 const OurCoursesSection = () => {
   return (
     <div className="h-fit">
@@ -32,195 +27,78 @@ const OurCoursesSection = () => {
           <span>Start one of our highest quality</span>
           <span>courses from the country!</span>
         </h1>
-        <div className="flex h-fit w-full items-center justify-center gap-12 space-x-4 p-8 sm:flex-col md:flex-row">
+        <div className="flex h-fit w-full items-center justify-center gap-12 space-x-4 p-8 sm:flex-col-reverse md:flex-row">
           <div className="flex flex-col items-end gap-12 space-y-4 sm:h-1/2 md:w-1/2">
-            <h1 className="flex h-[10rem] w-fit flex-col border-r-8 border-[#94C025] p-2">
-              <span className="text-3xl">Create Online </span>
-              <span className="text-3xl">Courses</span>
-              <span className="text-sm">Learn in- demand skills that</span>
-              <span className="text-sm">
-                prepare you for an entry level job.
+            <h1 className="flex h-fit w-fit flex-col gap-4 font-[Prata] text-[#233D63] lg:pl-36">
+              <span className="flex gap-4 font-bold sm:text-lg md:text-xl lg:text-3xl">
+                <div className="mt-2 flex items-start justify-center text-center text-xl text-[#94C025]">
+                  <BiSolidCircle />
+                </div>
+                Course is designed for 16 weeks per level on an average
+              </span>
+
+              <span className="flex gap-4 font-bold sm:text-lg md:text-xl lg:text-3xl">
+                <div className="mt-2 flex items-start justify-center text-center text-xl text-[#94C025]">
+                  <BiSolidCircle />
+                </div>
+                Weekly 2 hrs. class
+              </span>
+              <span className="flex gap-4 font-bold sm:text-lg md:text-xl lg:text-3xl">
+                <div className="mt-2 flex items-start justify-center text-center text-xl text-[#94C025]">
+                  <BiSolidCircle />
+                </div>
+                Teacher Student ratio 1:10
               </span>
             </h1>
-            <div className="border-8 border-[#233D63]">
-              <Image
-                src={img1}
-                alt=""
-                className="h-[310px] w-[299px] p-[10px]"
-              />
+            <div className="">
+              <Image src={img2} alt="" className="h-fit w-fit" />
             </div>
-            <h1 className="flex h-[10rem] w-fit flex-col border-r-8 border-[#94C025] p-2">
-              <span className="text-3xl">Deliver </span>
-              <span className="text-3xl">learning</span>
-              <span className="text-sm">Learn in- demand skills that</span>
-              <span className="text-sm">
-                prepare you for an entry level job.
-              </span>
-            </h1>
           </div>
           <div className="flex flex-col items-start gap-12 space-y-4 md:w-1/2">
-            <div className="border-8 border-[#233D63]">
-              <Image
-                src={img2}
-                alt=""
-                className="h-[310px]  w-[299px] p-[10px]"
-              />
+            <div className="">
+              <Image src={img1} alt="" className="h-fit w-fit " />
             </div>
-            <h1 className="flex h-[10rem] w-fit flex-col  border-l-8 border-[#94C025] p-2">
-              <span className="mb-4 text-3xl">Tutoring Online </span>
-
-              <span className="text-sm">Learn in- demand skills that</span>
-              <span className="text-sm">
-                prepare you for an entry level job.
+            <h1 className="flex h-fit w-[80%] flex-col  border-l-4 border-[#94C025] p-2">
+              <span className="mb-4 font-[Prata] text-[#94C025] sm:text-lg md:text-xl lg:text-3xl">
+                Level wise Performance Certificates are award after successful
+                completion of each level
               </span>
             </h1>
-            <div className="border-8 border-[#233D63]">
-              <Image
-                src={img3}
-                alt=""
-                className="h-[310px] w-[299px] p-[10px]"
-              />
-            </div>
           </div>
         </div>
       </div>
-      <div className="h-fit w-full p-8">
-        <h1 className="flex flex-col  items-center justify-center text-2xl font-bold text-[#233D63]">
-          <span>Popular</span>
-          <span>Certificates</span>
+      <div className="flex w-full flex-col items-start gap-4 p-8 lg:pl-[17rem]">
+        <div className="flex w-fit border bg-[#D9D9D9] font-[Prata] text-3xl font-bold text-[#233D63]">
+          <h1>Activities in Abacus Class</h1>
+        </div>
+        <div className="flex flex-col gap-2 font-[Gilroy-Regular] text-2xl text-[#233D63] ">
+          <span>1) 100% time framed fun activity based class</span>
+          <span>
+            2) Students solve exercises from their book within fixed time
+          </span>
+          <span>
+            3) Through Flash Cards , students identify numbers seeing image on
+            cards
+          </span>
+          <span>4) Speed Writing Activities on numbers within a minute</span>
+          <span>5) Oral Exercises on calculation are conducted</span>
+        </div>
+      </div>
+      <div className="flex h-fit w-full flex-col bg-[#004E65] p-20">
+        <h1 className="font-[Prata] text-white sm:text-2xl  lg:text-4xl">
+          BENEFITS OF PROIQ ABACUS
         </h1>
-        <div className="flex flex-col">
-          <div className=" flex h-fit w-full items-center justify-center sm:flex-col sm:space-y-4 sm:p-8 md:flex-col md:space-y-4 md:p-16 lg:flex-row lg:space-x-4">
-            <div className="flex h-[20rem] w-[20rem] flex-col items-center justify-center space-y-16  ">
-              <div className="flex h-[6rem] w-[6rem]  rounded-lg bg-[#C0EC33]">
-                <Image src={icon1} alt="" className="h-[90px] w-[90px] " />
-              </div>
-              <div className="flex flex-col space-y-2">
-                <span className="font-[Palanquin Dark] text-lg font-bold text-[#233D63]">
-                  It Support
-                </span>
-                <span className="font-[Palanquin] text-sm text-[#485261] ">
-                  Learn in- demand skills that prepare you for an entry level
-                  job.
-                </span>
-                <div className="flex justify-between">
-                  <button className="text-md flex flex-row font-semibold text-[#233D63] hover:text-sm hover:font-bold hover:text-[#94C025]">
-                    READ MORE
-                    <BsArrowRight />
-                  </button>
-                  <p>$60</p>
-                </div>
-              </div>
-            </div>
-            <div className="flex h-[20rem] w-[20rem] flex-col items-center justify-center space-y-16  ">
-              <div className="flex h-[6rem] w-[6rem]  rounded-lg bg-[#C0EC33]">
-                <Image src={icon2} alt="" className="h-[90px] w-[90px] " />
-              </div>
-              <div className="flex flex-col space-y-2">
-                <span className="font-[Palanquin Dark] text-lg font-bold text-[#233D63]">
-                  Mechine Learning
-                </span>
-                <span className="font-[Palanquin] text-sm text-[#485261] ">
-                  It is the best way to make progress towards human-level AI.
-                </span>
-                <div className="flex justify-between">
-                  <button className="text-md flex flex-row font-semibold text-[#233D63] hover:text-sm hover:font-bold hover:text-[#94C025]">
-                    READ MORE
-                    <BsArrowRight />
-                  </button>
-                  <p>$60</p>
-                </div>
-              </div>
-            </div>
-            <div className="flex h-[20rem] w-[20rem] flex-col items-center justify-center space-y-16  ">
-              <div className="flex h-[6rem] w-[6rem]  rounded-lg bg-[#C0EC33]">
-                <Image src={icon3} alt="" className="h-[90px] w-[90px] " />
-              </div>
-              <div className="flex flex-col space-y-2">
-                <span className="font-[Palanquin Dark] text-lg font-bold text-[#233D63]">
-                  python
-                </span>
-                <span className="font-[Palanquin] text-sm text-[#485261] ">
-                  Learn Python programming online with courses like Python for
-                  everybody.
-                </span>
-                <div className="flex justify-between">
-                  <button className="text-md flex flex-row font-semibold text-[#233D63] hover:text-sm hover:font-bold hover:text-[#94C025]">
-                    READ MORE
-                    <BsArrowRight />
-                  </button>
-                  <p>$60</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className=" flex h-fit w-full items-center justify-center  sm:flex-col sm:space-y-4 sm:p-8 md:flex-col md:space-y-4 md:p-16 lg:flex-row lg:space-x-8">
-              <div className="flex h-[20rem] w-[20rem] flex-col items-center justify-center space-y-16  ">
-                <div className="flex h-[6rem] w-[6rem]  rounded-lg bg-[#C0EC33]">
-                  <Image src={icon4} alt="" className="h-[90px] w-[90px] " />
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <span className="font-[Palanquin Dark] text-lg font-bold text-[#233D63]">
-                    Data Analyst
-                  </span>
-                  <span className="font-[Palanquin] text-sm text-[#485261] ">
-                    Gain the job ready skills for an entry-level data analyst.
-                  </span>
-                  <div className="flex justify-between">
-                    <button className="text-md flex flex-row font-semibold text-[#233D63] hover:text-sm hover:font-bold hover:text-[#94C025]">
-                      READ MORE
-                      <BsArrowRight />
-                    </button>
-                    <p>$60</p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex h-[20rem] w-[20rem] flex-col items-center justify-center space-y-16  ">
-                <div className="flex h-[6rem] w-[6rem]  rounded-lg bg-[#C0EC33]">
-                  <Image src={icon5} alt="" className="h-[90px] w-[90px] " />
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <span className="font-[Palanquin Dark] text-lg font-bold text-[#233D63]">
-                    Deep Learning
-                  </span>
-                  <span className="font-[Palanquin] text-sm text-[#485261] ">
-                    The Deep learning Specialization provides a pathway.
-                  </span>
-                  <div className="flex justify-between">
-                    <button className="text-md flex flex-row font-semibold text-[#233D63] hover:text-sm hover:font-bold hover:text-[#94C025]">
-                      READ MORE
-                      <BsArrowRight />
-                    </button>
-                    <p>$60</p>
-                  </div>
-                </div>
-              </div>
-              <div className="flex h-[20rem] w-[20rem] flex-col items-center justify-center space-y-16  ">
-                <div className="flex h-[6rem] w-[6rem]  rounded-lg bg-[#C0EC33]">
-                  <Image src={icon6} alt="" className="h-[90px] w-[90px] " />
-                </div>
-                <div className="flex flex-col space-y-2">
-                  <span className="font-[Palanquin Dark] text-lg font-bold text-[#233D63]">
-                    Data Science
-                  </span>
-                  <span className="font-[Palanquin] text-sm text-[#485261] ">
-                    Data Science courses or pay to earn a course or
-                    Specialization Certificate.
-                  </span>
-                  <div className="flex justify-between">
-                    <button className="text-md flex flex-row font-semibold text-[#233D63] hover:text-sm hover:font-bold hover:text-[#94C025]">
-                      READ MORE
-                      <BsArrowRight />
-                    </button>
-                    <p>$60</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <p className="pt-8 font-[Gilroy-Regular] text-white sm:text-lg  lg:text-2xl">
+          Now a days, your child's favorite cartoon characters on mobile can
+          lower his/her concentration to studies . Engaging him/her in Pro IQ
+          Abacus programme is directly associated with better results. Learning
+          is highly enjoyable with Pro IQ Abacus and removes fear of math in
+          kids. Pro IQ Abacus is a great support to help kids instilling Love
+          for Learning. Pro IQ Abacus also improves academic performance with
+          other skills such as:
+        </p>
       </div>
+      <OurCoursesSkill />
     </div>
   );
 };
