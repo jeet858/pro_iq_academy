6657083b1e939aaff8588913237936c8ca2461e3
Achinta27 <achinta27sep1999@gmail.com>
2023-10-08 00:19:18 +0530

    modified

--- src/components/elements/StudentCommunity.tsx
+++ src/components/elements/StudentCommunity.tsx
@@ -1,57 +1,110 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import SectionHeader from "./SectionHeader";
 import StudentBox from "./StudentBox";
-import { api } from "~/utils/api";
 
 const StudentCommunity: React.FC = () => {
-  const { data: reviews, isError, isLoading } = api.review.all.useQuery();
-  const [data, setData] = useState({ name: "", email: "", review: "" });
+  const [slidesToShow, setSlidesToShow] = useState(3);
 
-  if (isError) {
-    return (
-      <div
-        className=" w-full items-center justify-center bg-[length:100%_100%]   bg-no-repeat sm:h-fit sm:p-8 md:h-fit md:p-8 lg:h-full lg:p-32"
-        style={{ backgroundImage: 'url("./images/community_background.png") ' }}
-      >
-        <div className="h-44">
-          <SectionHeader title="Student Community" subtitle="Feedback" />
-        </div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 sm:gap-y-16 md:grid-cols-2 lg:grid-cols-3"></div>
-      </div>
-    );
-  } else if (isLoading) {
-    return (
-      <div
-        className=" w-full items-center justify-center bg-[length:100%_100%]   bg-no-repeat sm:h-fit sm:p-8 md:h-fit md:p-8 lg:h-full lg:p-32"
-        style={{ backgroundImage: 'url("./images/community_background.png") ' }}
-      >
-        <div className="h-44">
-          <SectionHeader title="Student Community" subtitle="Feedback" />
-        </div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 sm:gap-y-16 md:grid-cols-2 lg:grid-cols-3"></div>
-      </div>
-    );
-  }
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 640) {
+        setSlidesToShow(1);
+      } else if (window.innerWidth <= 1024) {
+        setSlidesToShow(2);
+      } else {
+        setSlidesToShow(3);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const studentData = [
+    {
+      name: "Mohan Singh",
+      role: "Parent",
+      imageSrc: "/images/image-3.jpg",
+      description:
+        "PRO IQ Academy has transformed my child's abilities with their mental math and skill development program. The instructors are experienced, and the small batches ensure personalized attention. Affordable fees and online/offline classes make it convenient.",
+    },
+    {
+      name: "Shalini Sharma",
+      role: "Parent",
+      imageSrc: "/images/image-3.jpg",
+      description:
+        "Choosing PRO IQ Academy was the best decision for my child's education. Their focus on reading, writing, and observation skills has improved overall intelligence. The instructors are skilled, and the evaluations are fun and engaging.",
+    },
+    {
+      name: "Tarun Shrivastava",
+      role: "Parent",
+      imageSrc: "/images/image-3.jpg",
+      description:
+        "PRO IQ Academy offers an excellent learning experience. My child's numerical ability and critical thinking skills have improved significantly. The academy's emphasis on clean handwriting and English communication has boosted confidence. Highly recommended!.",
+    },
+    {
+      name: "Mohan Singh",
+      role: "Parent",
+      imageSrc: "/images/image-3.jpg",
+      description:
+        "PRO IQ Academy has transformed my child's abilities with their mental math and skill development program. The instructors are experienced, and the small batches ensure personalized attention. Affordable fees and online/offline classes make it convenient.",
+    },
+    {
+      name: "Shalini Sharma",
+      role: "Parent",
+      imageSrc: "/images/image-3.jpg",
+      description:
+        "Choosing PRO IQ Academy was the best decision for my child's education. Their focus on reading, writing, and observation skills has improved overall intelligence. The instructors are skilled, and the evaluations are fun and engaging.",
+    },
+    {
+      name: "Tarun Shrivastava",
+      role: "Parent",
+      imageSrc: "/images/image-3.jpg",
+      description:
+        "PRO IQ Academy offers an excellent learning experience. My child's numerical ability and critical thinking skills have improved significantly. The academy's emphasis on clean handwriting and English communication has boosted confidence. Highly recommended!.",
+    },
+  ];
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: slidesToShow,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+  };
+
   return (
     <div
-      className=" w-full items-center justify-center bg-[length:100%_100%]   bg-no-repeat sm:h-fit sm:p-8 md:h-fit md:p-8 lg:h-full lg:p-32"
-      style={{ backgroundImage: 'url("./images/community_background.png") ' }}
+      className="w-full items-center justify-center bg-[length:100%_100%] bg-no-repeat sm:h-fit sm:p-8 md:h-fit md:p-8 lg:h-full lg:p-32"
+      style={{ backgroundImage: 'url("./images/community_background.png")' }}
     >
-      <div className="h-44">
+      <div className="h-36">
         <SectionHeader title="Student Community" subtitle="Feedback" />
       </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 sm:gap-y-16 md:grid-cols-2 lg:grid-cols-3">
-        {reviews.map((review, index) => {
-          return (
-            <StudentBox
-              key={index}
-              name={review.name}
-              email={review.email}
-              imageSrc="/images/image-3.jpg"
-              description={review.review}
-            />
-          );
-        })}
+      <div className="grid grid-cols-1 gap-4">
+        <Slider {...sliderSettings}>
+          {studentData.map((student, index) => (
+            <div key={index} className="mt-12">
+              <StudentBox
+                name={student.name}
+                email={student.role}
+                imageSrc={student.imageSrc}
+                description={student.description}
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
